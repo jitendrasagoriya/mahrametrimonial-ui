a26cfd8d593fb37dc24ccaef25b7ea51115cc20f
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2018-02-24 11:29:54 +0530

    Added login and Auth Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { AuthService } from './services/auth.service';
+import { RequestInterceptorService } from './services/request-interceptor.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: RequestInterceptorService, multi: true },
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
